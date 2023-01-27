a25d4614d3cd52640ef520639ddcf28d349aeb66
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-27 19:29:40 +0100

    created user model

---
+++ src/model/user.model.ts
@@ -0,0 +1,45 @@
+import { model, Schema, Document, CallbackWithoutResultAndOptionalError } from 'mongoose';
+import moment from 'moment';
+import bcrypt from "bcrypt"
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+      },
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+      },
+      password: {
+        type: String,
+        required: true,
+        select:false,
+      },
+      isVerified: {
+        type: Boolean,
+        default: false,
+      },
+      joinedAt: {
+        type: Date,
+        default: () => moment().toDate(),
+      },
+})
+
+userSchema.pre('save', async function (next: CallbackWithoutResultAndOptionalError) {
+    if (!this.isModified("password")) next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  })
+  
+  userSchema.methods.isPasswordMatch = async function (password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password)
+  }
+  
+  
+  
+  const userModel = model<Document>('User', userSchema);
+  export default userModel;
