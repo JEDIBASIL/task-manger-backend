12c9d8c9fc47373f6796fc6d4828e876d0f02cb5
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-27 21:13:12 +0100

    added middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express"
 import logger from "./utils/logger";
+import { dbConnection } from "./database";
+import { ConnectOptions, connect } from "mongoose";
 class App {
     private app: express.Application;
     private port: number;
     constructor() {
         this.app = express();
         this.port = 8084;
+        this.databaseConnection(); 
+        this.initializeMiddleware()
     }
     listen() {
         this.app.listen(this.port, () => {
@@ -15,7 +19,24 @@ class App {
             logger.info(`▼                                   ▼`)
             logger.info("▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎")
         })
-        
+    }
+
+    private async databaseConnection() {
+        try {
+            await connect(dbConnection.uri as string, dbConnection.options as ConnectOptions)
+            logger.info("▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎")
+            logger.info(`▼                                   ▼`)
+            logger.info(`▼ 🛢  🅳🅰🆃🅰🅱🅰🆂🅴 🅲🅾🅽🅽🅴🅲🆃🅴🅳             ▼`)
+            logger.info(`▼                                   ▼`)
+            logger.info("▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎")
+        } catch (err: unknown) {
+            if (err instanceof Error) console.log(err.message)
+        }
+    }
+
+    private initializeMiddleware() {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
     }
 }
 
